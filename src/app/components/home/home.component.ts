f95db9a23f4d52dbdd73a7e59515caa633fb1f21
juan.faundez <juan.faundez@valposystem.com>
2024-11-08 13:14:48 -0300

    Fixed file min length validation

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MainServiceService } from '../../services/main-service.service';
+import * as XLSX from 'xlsx';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-home',
@@ -29,11 +31,23 @@ export class HomeComponent implements OnInit {
     this.selectedFile = event.target.files[0];
   }
 
-  uploadFile() {
+  async uploadFile() {
     if (!this.selectedFile) {
       alert('No file selected');
       return;
     }
+
+    const isValid = await this.validFileLength();
+    if (!isValid) {
+      Swal.fire({
+        title: 'Error!',
+        text: 'El archivo debe contener al menos 5 registros.',
+        icon: 'error',
+        confirmButtonText: 'Cerrar'
+      })
+      return;
+    }
+
     const formData = new FormData();
     formData.append('file', this.selectedFile);
     this.mainService.uploadFile(formData).subscribe(
@@ -56,6 +70,38 @@ export class HomeComponent implements OnInit {
         console.log(err);
       }
     )
+  }
+
+  validFileLength(): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      if (!this.selectedFile) {
+        resolve(false);
+        return;
+      }
+
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        const data = new Uint8Array(e.target.result);
+        const workbook = XLSX.read(data, { type: 'array' });
+        const firstSheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[firstSheetName];
+        const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+
+        const dataWithoutHeaders = jsonData.slice(1);
+
+        const filteredData = dataWithoutHeaders.filter((row: any) => {
+          return row.some((cell: any) => cell !== null && cell !== undefined && cell !== '');
+        });
+        console.log(filteredData.length);
+        if (filteredData.length < 5) {
+          resolve(false);
+        } else {
+          resolve(true);
+        }
+      };
+      reader.onerror = (error) => reject(error);
+      reader.readAsArrayBuffer(this.selectedFile);
+    });
   }
 
 }
